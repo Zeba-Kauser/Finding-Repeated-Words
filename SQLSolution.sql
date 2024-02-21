30750aa6cf5ca7aee5470716ef9fc1291a546147
Zeba-Kauser <155068390+Zeba-Kauser@users.noreply.github.com>
2024-02-21 15:20:15 +0530

    Update SQLSolution.sql

--- SQLSolution.sql
+++ SQLSolution.sql
@@ -1,17 +1,17 @@
---Here is the script 
+--create the table
 create table namaste_python (
 file_name varchar(25),
 content varchar(200)
 );
 
-delete from namaste_python;
+--insert data into the table
 insert into namaste_python values ('python bootcamp1.txt','python for data analytics 0 to hero bootcamp starting on Jan 6th')
 ,('python bootcamp2.txt','classes will be held on weekends from 11am to 1 pm for 5-6 weeks')
 ,('python bootcamp3.txt','use code NY2024 to get 33 percent off. You can register from namaste sql website. Link in pinned comment');
 
 select * from namaste_python;
 
---Solution
+--Solution:
 With Iteams as 
 (select * 
 from namaste_python
